9f7ee52d855a2c1ab8cd9acfe12ade09133863bc
Martin Wickham <spexguy070@gmail.com>
2014-02-08 09:48:10 -0600

    added client-side player and necessity classes

---
+++ voxel-client/clientnecessity.js
@@ -0,0 +1,14 @@
+require('../necessity.js');
+
+function ClientNecessity(baseNecessity, domEl) {
+	this.lastTimeRefreshed = baseNecessity.lastTimeRefreshed;
+	this.timeToDeplete = baseNecessity.timeToDeplete;
+	this.domEl = domEl;
+}
+
+ClientNecessity.prototype = new Necessity();
+
+ClientNecessity.prototype.update = function() {
+	Necessity.prototype.update.call(this); //super.update();
+	this.domEl.innerHTML = ""+this.getValue();
+}
---
+++ voxel-client/clientplayer.js
@@ -0,0 +1,17 @@
+require('../player.js');
+require('./clientnecessity.js');
+
+function ClientPlayer(healthEl, heatEl, sleepEl) {
+	Player.call(this); //call superconstructor
+	this.healthEl = healthEl;
+	this.heat = new ClientNecessity(this.heat, heatEl);
+	this.sleep = new ClientNecessity(this.sleep, sleepEl);
+}
+
+ClientPlayer.prototype = new Player();
+
+ClientPlayer.prototype.update = function() {
+	this.healthEl.innerHTML = ""+this.health;
+	this.heat.update();
+	this.sleep.update();
+}
