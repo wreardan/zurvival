b534ac974677eaddd59f1f35423fa5baf623b429
Wesley Reardan <reardan@gmail.com>
2014-02-09 03:18:40 -0500

    from russia with love

--- voxel-client/www/demo.js
+++ voxel-client/www/demo.js
@@ -1,4 +1,12 @@
-if(document.URL.search("localhost") == -1)
+var url
+
+url = "ws://"+window.location.host;
+url = url.replace(/^https?:\/\//,'')
+
+require('./hello-world.js')({server: url})
+/*
+if(document.URL.search("localhost") !== -1)
 	require('./hello-world.js')({server: "ws://test.worldwebcraft.com/"})
 else
 	require('./hello-world.js')({server: "ws://localhost:80/"})
+*/
